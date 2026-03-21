efdaed05e343b22f91a853f326a3d5a19b701b69
Franco <frfantusatti@protonmail.com>
2026-03-21 18:29:41 -0300

    fix error on deploy, import request

--- flaskr/bookmarks.py
+++ flaskr/bookmarks.py
@@ -6,7 +6,7 @@ from werkzeug.exceptions import abort
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
-import requests
+from flask import request 
 from bs4 import BeautifulSoup
 
 bp = Blueprint('bookmarks', __name__)
